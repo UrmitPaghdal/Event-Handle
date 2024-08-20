ab101e58c39a4a220cf84d83e5e65ddf06edbb5e
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-08-20 17:26:44 +0530

    React JS

---
+++ Event Handler/src/Data.js
@@ -0,0 +1,223 @@
+const category = [
+    {
+        id : 1,
+        name : "Kia"
+    },
+    {
+        id :2,
+        name : "Mahindra"
+    },
+    {
+        id : 3,
+        name : "Toyota"
+    },
+    {
+        id : 4,
+        name : "Mg"
+    },
+    {
+        id : 5,
+        name : "Tata"
+    },
+    {
+        id : 6,
+        name : "Rolls Royce"
+    },
+    {
+        id : 7,
+        name : "Porsche"
+    }
+]
+
+const Item = [
+    {
+        id : 1,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/141383/sonet-facelift-exterior-right-front-three-quarter-8.jpeg?isig=0&q=80",
+        name : "Sonet",
+        price : "1,44,900",
+        category : "Kia"
+    },
+    {
+        id : 2,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/40087/thar-exterior-right-front-three-quarter-35.jpeg?isig=0&q=80",
+        name : "Thar",
+        price : "41,999",
+        category : "Mahindra"
+    },
+    {
+        id : 3,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/115025/innova-hycross-exterior-right-front-three-quarter-73.jpeg?isig=0&q=80",
+        name : "Innova Hycross",
+        price : "1,29,000",
+        category : "Toyota"
+    },
+    {
+        id : 4,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/130583/hector-exterior-right-front-three-quarter-75.jpeg?isig=0&q=80",
+        name : "Hector",
+        price : "99,900",
+        category : "Mg"
+    },
+    {
+        id : 5,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/149123/nexon-ev-exterior-right-front-three-quarter-3.jpeg?isig=0&q=80",
+        name : "Nexon EV",
+        price : "1,34,900",
+        category : "Tata"
+    },
+    {
+        id : 6,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/168715/spectre-exterior-right-front-three-quarter-3.jpeg?isig=0&q=80",
+        name : "Spectre",
+        price : "19,900",
+        category : "Rolls Royce"
+    },
+    {
+        id : 7,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/169989/macan-turbo-ev-exterior-right-front-three-quarter.jpeg?isig=0&q=80",
+        name : "Macan Turbo EV",
+        price : "89,900",
+        category : "Porsche"
+    },
+    {
+        id : 8,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/114971/ev6-exterior-right-front-three-quarter-4.jpeg?isig=0&q=80",
+        name : "EV6",
+        price : "29,900",
+        category : "Kia"
+    },
+    {
+        id : 9,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/131907/xuv300-turbosport-exterior-right-front-three-quarter-9.jpeg?isig=0&q=80",
+        name : "XUV300 TurboSport",
+        price : "85,900",
+        category : "Mahindra"
+    },
+    {
+        id : 10,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/139739/land-cruiser-exterior-right-front-three-quarter-2.jpeg?isig=0&q=80",
+        name : "Land Cruiser",
+        price : "78,999",
+        category : "Toyota"
+    },
+    {
+        id : 11,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/125193/comet-ev-exterior-right-side-view-23.jpeg?isig=0&q=80",
+        name : "Comet EV",
+        price : "24,900",
+        category : "Mg"
+    },
+    {
+        id : 12,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/138895/safari-facelift-exterior-right-front-three-quarter-39.jpeg?isig=0&q=80",
+        name : "Safari",
+        price : "69,990",
+        category : "Tata"
+    },
+    {
+        id : 13,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/30181/phantom-exterior-right-front-three-quarter.jpeg?isig=0&q=80",
+        name : "Phantom",
+        price : "59,900",
+        category : "Rolls Royce"
+    },
+    {
+        id : 14,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/45063/taycan-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80",
+        name : "Taycan",
+        price : "41,900",
+        category : "Porsche"
+    },
+    {
+        id : 15,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/144163/carens-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80",
+        name : "Carens",
+        price : "54,990",
+        category : "kia"
+    },
+    {
+        id : 16,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/40432/scorpio-n-exterior-right-front-three-quarter-75.jpeg?isig=0&q=80",
+        name : "Scorpio N",
+        price : "1,69,900",
+        category : "Mahindra"
+    },
+    {
+        id : 17,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-20.jpeg?isig=0&q=80",
+        name : "Fortuner",
+        price : "41,900",
+        category : "Toyota"
+    },
+    {
+        id : 18,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/139315/hector-plus-exterior-right-front-three-quarter-2.jpeg?isig=0&q=80",
+        name : "Hector Pluse",
+        price : "32,990",
+        category : "Mg"
+    },
+    {
+        id : 19,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/141867/nexon-exterior-right-front-three-quarter-71.jpeg?isig=0&q=80",
+        name : "Nexon",
+        price : "19,500",
+        category : "Tata"
+    },
+    {
+        id : 20,
+        img : "https://imgd.aeplcdn.com/664x374/cw/ec/34427/RollsRoyce-Cullinan-Exterior-158729.jpg?wm=0&q=80",
+        name : "Cullinan",
+        price : "1,59,900",
+        category : "Rolls Royce"
+    },
+    {
+        id : 21,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/165641/panamera-exterior-right-front-three-quarter.jpeg?isig=0&q=80",
+        name : "Panamera",
+        price : "39,499",
+        category : "Porsche"
+    },
+    {
+        id : 22,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/135051/seltos-exterior-right-front-three-quarter-31.jpeg?isig=0&q=80",
+        name : "Seltos",
+        price : "89,990",
+        category : "Kia"
+    },
+    {
+        id : 23,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/156405/xuv-3xo-exterior-right-front-three-quarter-33.jpeg?isig=0&q=80",
+        name : "XUV 3XO",
+        price : "52,000",
+        category : "Mahindra"
+    },
+    {
+        id : 24,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/140809/innova-crysta-exterior-right-front-three-quarter-2.png?isig=0&q=80",
+        name : "Innova Crysta",
+        price : "49,546",
+        category : "Toyota"
+    },
+    {
+        id : 25,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/110437/zs-ev-exterior-right-front-three-quarter-69.jpeg?isig=0&q=80",
+        name : "ZS EV",
+        price : "1,16,000",
+        category : "Mg"
+    },
+    {
+        id : 26,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/40453/tiago-ev-exterior-right-front-three-quarter-11.jpeg?isig=0&q=80",
+        name : "Tiago EV",
+        price : "1,16,000",
+        category : "Tata"
+    },
+    {
+        id : 27,
+        img : "https://imgd.aeplcdn.com/664x374/n/cw/ec/24421/718-exterior-right-front-three-quarter-90912.jpeg?isig=0&q=80",
+        name : "718",
+        price : "1,16,000",
+        category : "Porsche"
+    },
+]
+export {category,Item}
